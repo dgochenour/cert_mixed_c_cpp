e58ff58287579e6200af0444b3f392c28deebb8d
Don Gochenour <don@rti.com>
2021-05-06 11:43:01 -0400

    remove literals, moved to common_config

--- example_publisher.cxx
+++ example_publisher.cxx
@@ -73,53 +73,54 @@ int main(void)
     if (udp_property == NULL) {
         std::cout << "ERROR: failed to allocate udp properties" << std::endl;
     }
+
     *udp_property = UDP_INTERFACE_FACTORY_PROPERTY_DEFAULT;
     udp_property->disable_auto_interface_config = RTI_TRUE;
 
-    if (!DDS_StringSeq_set_maximum(&udp_property->allow_interface,2))
-    {
+    if (!DDS_StringSeq_set_maximum(&udp_property->allow_interface,2)) {
         printf("failed to set allow_interface maximum\n");
         return -1;
     }
-    if (!DDS_StringSeq_set_length(&udp_property->allow_interface,2))
-    {
+    if (!DDS_StringSeq_set_length(&udp_property->allow_interface,2)) {
         printf("failed to set allow_interface length\n");
         return -1;
     }
 
-    if (!UDP_InterfaceTableEntrySeq_set_maximum(&udp_property->if_table,2))
-    {
+    if (!UDP_InterfaceTableEntrySeq_set_maximum(&udp_property->if_table,2)) {
         printf("failed to set if_table maximum\n");
         return -1;
     }
 
     *DDS_StringSeq_get_reference(&udp_property->allow_interface,0) = 
-            DDS_String_dup("loopback");
+            DDS_String_dup(k_loopback_name.c_str());
     if (!UDP_InterfaceTable_add_entry(
             &udp_property->if_table,
-            0x7f000001,
-            0xff000000,
-            "loopback",
-            UDP_INTERFACE_INTERFACE_UP_FLAG))
-
+            k_loopback_ip,
+            k_loopback_mask,
+            k_loopback_name.c_str(),
+            UDP_INTERFACE_INTERFACE_UP_FLAG)) 
     {
-        printf("failed to add 'loopback' interface\n");
+        std::cout << "ERROR: failed to add " << k_loopback_name.c_str() <<
+                "interface" << std::endl;
         return -1;
     }
 
     *DDS_StringSeq_get_reference(&udp_property->allow_interface,1) = 
-            DDS_String_dup("real_nic");
+            DDS_String_dup(k_real_nic_name.c_str());
     if (!UDP_InterfaceTable_add_entry(
             &udp_property->if_table,
-            0xc0a80174, //192.168.1.116
-            0xffffff00, //255.255.255.0
-            "real_nic",
-            UDP_INTERFACE_INTERFACE_UP_FLAG))
-
+            k_real_nic_ip,
+            k_real_nic_mask,
+            k_real_nic_name.c_str(),
+            UDP_INTERFACE_INTERFACE_UP_FLAG)) 
     {
-        printf("failed to add 'real_nic' interface\n");
+        std::cout << "ERROR: failed to add " << k_loopback_name.c_str() <<
+                "interface" << std::endl;
         return -1;
     }
+
+    //explicitly set the "real NIC" as the multicast interface
+    udp_property->multicast_interface = DDS_String_dup(k_real_nic_name.c_str());
 
     if(!RT_Registry_register(
             registry, 
